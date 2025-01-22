e8f6fb72d1dd9c42822f5acb367a93f68b0de45b
Ryan Taylor <rctaylor99@outlook.com>
2025-01-21 21:22:10 -0800

    adding initial python file for scanning document

---
+++ scan.py
@@ -0,0 +1,8 @@
+# importing packages
+from pyimagesearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils #TODO: check installation procedure for MacOS
+
