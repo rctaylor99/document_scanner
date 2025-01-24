8ac95c3841d063993873f6544852920612e50406
Ryan Taylor <rctaylor99@outlook.com>
2025-01-23 20:34:37 -0800

    added test image and functions to read and resize test image

--- scan.py
+++ scan.py
@@ -1,8 +1,106 @@
 # Importing packages
-from pyimagesearch.transform import four_point_transform
-from skimage.filters import threshold_local
+# from pyimagesearch.transform import four_point_transform
+# from skimage.filters import threshold_local
 import numpy as np
 import argparse
 import cv2
 import imutils 
 
+# Defining constants
+resize_width = 1200
+resize_height = 800
+
+# KEEPING THE FOLLOWING FUNCTION AS REFERENCE
+# def order_points(pts):
+#     # initialzie a list of coordinates that will be ordered
+#     # such that the first entry in the list is the top-left,
+#     # the second entry is the top-right, the third is the
+#     # bottom-right, and the fourth is the bottom-left
+#     rect = np.zeros((4, 2), dtype = "float32")
+
+#     # the top-left point will have the smallest sum, whereas
+#     # the bottom-right point will have the largest sum
+#     s = pts.sum(axis = 1)
+#     rect[0] = pts[np.argmin(s)]
+#     rect[2] = pts[np.argmax(s)]
+
+#     # now, compute the difference between the points, the
+#     # top-right point will have the smallest difference,
+#     # whereas the bottom-left will have the largest difference
+#     diff = np.diff(pts, axis = 1)
+#     rect[1] = pts[np.argmin(diff)]
+#     rect[3] = pts[np.argmax(diff)]
+
+#     # return the ordered coordinates
+#     return rect
+
+# KEEPING THE FOLLOWING FUNCTION AS REFERENCE
+# def four_point_transform(image, pts):
+#     # obtain a consistent order of the points and unpack them
+#     # individually
+#     rect = order_points(pts)
+#     (tl, tr, br, bl) = rect
+
+#     # compute the width of the new image, which will be the
+#     # maximum distance between bottom-right and bottom-left
+#     # x-coordiates or the top-right and top-left x-coordinates
+#     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+#     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+#     maxWidth = max(int(widthA), int(widthB))
+
+#     # compute the height of the new image, which will be the
+#     # maximum distance between the top-right and bottom-right
+#     # y-coordinates or the top-left and bottom-left y-coordinates
+#     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+#     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+#     maxHeight = max(int(heightA), int(heightB))
+
+#     # now that we have the dimensions of the new image, construct
+#     # the set of destination points to obtain a "birds eye view",
+#     # (i.e. top-down view) of the image, again specifying points
+#     # in the top-left, top-right, bottom-right, and bottom-left
+#     # order
+#     dst = np.array([
+#         [0, 0],
+#         [maxWidth - 1, 0],
+#         [maxWidth - 1, maxHeight - 1],
+#         [0, maxHeight - 1]], dtype = "float32")
+
+#     # compute the perspective transform matrix and then apply it
+#     M = cv2.getPerspectiveTransform(rect, dst)
+#     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+#     # return the warped image
+#     return warped
+
+
+def scan_image():
+    # TODO: Update the function to scan, rather than return the test image in the folder 
+    return cv2.imread("test_image.jpg")
+
+
+def resize_image(image):
+    # TODO: Update the image_width and image_height variables to more reasonable values
+    return cv2.resize(image, (resize_width,resize_height))
+
+
+def main():
+    # Printing welcome message
+    print('===================================')
+    print('----Welcome to Document Scanner----')
+    print('===================================')
+
+    # Read in new image and resize to work with OpenCV
+    original_img = scan_image()
+    resized_img = resize_image(original_img)
+    
+
+    cv2.imshow("Image", resized_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return
+
+
+if __name__=="__main__": 
+    main()
