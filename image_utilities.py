a0dd6eea07eab8ace58d92ee3a646f14c440d80c
Ryan Taylor <rctaylor99@outlook.com>
2025-01-25 11:45:58 -0800

    tested the corner-matching

--- image_utilities.py
+++ image_utilities.py
@@ -1,10 +1,11 @@
 # Utilities that are useful for vision project, but don't need to be housed in the main file
 
 import cv2
+import numpy as np
 
 def scan_image():
     # TODO: Update the function to scan, rather than return the test image in the folder 
-    return cv2.imread("test_img.jpg")
+    return cv2.imread("test_img2.jpg")
 
 
 def resize_image(image, width, height):
@@ -25,6 +26,21 @@ def blur(image, kernel_size, sigma):
 def edge_detect(image, min, max):
     # Using Canny method of determining edges on an image
     return cv2.Canny(image, min, max)
+
+
+def map_endpoints(image):
+    image = image.reshape((4,2))
+    new_image = np.zeros((4,2), dtype = np.float32)
+
+    add = image.sum(1)
+    new_image[0] = image[np.argmin(add)]
+    new_image[2] = image[np.argmax(add)]
+
+    difference = np.diff(image, axis = 1)
+    new_image[1] = image[np.argmin(difference)]
+    new_image[3] = image[np.argmax(difference)]
+
+    return new_image
 
 def show_image(image):
     # Show the image to the screen
--- main.py
+++ main.py
@@ -12,8 +12,13 @@ blur_kernel_size = 5
 blur_sigma = 0
 
 # TODO: Edit these values to ensure best image quality
-edge_min = 95
-edge_max = 125
+edge_min = 150
+edge_max = 200
+
+# TODO: change this based on the size of the paper that is input
+base_point = 0
+input_width = 800
+input_height = 800
 
 
 def main():
@@ -32,10 +37,50 @@ def main():
     # Using Gaussian blur to eliminate the Gaussian noise from the image
     blurred_img = utils.blur(greyscale_img, blur_kernel_size, blur_sigma)
 
+    # Finding the edges of the image using Canny method
     edges_img = utils.edge_detect(blurred_img, edge_min, edge_max)
 
-    # USED IN TESTING -- Displaying resized image that will be used with OpenCV
     utils.show_image(edges_img)
+
+    # # Finding the corners of the paper, using the contours of the edges we just created
+    contours, hierarchy = cv2.findContours(edges_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    for c in contours:
+        p = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c,0.02 * p, True)
+
+        if len(approx)==4:
+            target = approx
+            break
+    approx = utils.map_endpoints(target) # approximate corners of the sheet
+
+    # Re-mapping points to specified size
+    corner_points = np.float32([[base_point,base_point], [input_width,base_point], [input_width,input_height], [base_point,input_height]])
+
+    approx_x: int = approx[0][0]
+    approx_y: int = approx[0][1]
+
+    print(approx_x)
+    print(approx_y)
+    print('------------')
+    print(approx)
+    print('------------')
+    print(corner_points)
+
+
+
+    circled_image = cv2.circle(edges_img, (211, 270), 10, (0, 0, 255), 5)
+    circled_image = cv2.circle(circled_image, (1018, 267), 10, (0, 0, 255), 5)
+    circled_image = cv2.circle(edges_img, (1024, 501), 10, (0, 0, 255), 5)
+    circled_image = cv2.circle(edges_img, (206, 498), 10, (0, 0, 255), 5)
+    utils.show_image(circled_image)
+
+    remapped_img = cv2.getPerspectiveTransform(approx, corner_points)
+    final_image = cv2.warpPerspective(original_img, remapped_img, [input_width, input_height])
+
+    # USED IN TESTING -- Displaying resized image that will be used with OpenCV
+    utils.show_image(remapped_img)
 
     return
 
